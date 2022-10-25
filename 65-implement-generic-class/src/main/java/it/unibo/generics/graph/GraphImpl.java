7bffb332d46131d12f2ae480073af7fa411a54a2
mattia <flami2002@gmail.com>
2022-10-26 00:47:45 +0200

    implement nodeSet && addNode

--- 65-implement-generic-class/src/main/java/it/unibo/generics/graph/GraphImpl.java
+++ 65-implement-generic-class/src/main/java/it/unibo/generics/graph/GraphImpl.java
@@ -1,5 +1,45 @@
 package it.unibo.generics.graph;
 
-public class GraphImpl {
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import it.unibo.generics.graph.api.Graph;
+
+public class GraphImpl<N> implements Graph<N> {
+
+    private final Map<N, Set<N>> edges = new LinkedHashMap<>();
+
+
+    @Override
+    public void addNode(final N node) {
+        edges.putIfAbsent(node, new HashSet<>());
+    
+    }
+
+    @Override
+    public void addEdge(Object source, Object target) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public Set<N> nodeSet() {
+        return new HashSet<>(edges.keySet());
+    }
+
+    @Override
+    public Set<N> linkedNodes(Object node) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List getPath(Object source, Object target) {
+        // TODO Auto-generated method stub
+        return null;
+    }
     
 }
